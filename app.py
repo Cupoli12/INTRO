1c782bf66c9e12fbdd40a2dd50496c5a8aff51c2
Cupoli12 <167145381+Cupoli12@users.noreply.github.com>
2024-08-15 10:27:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,27 @@ st.title("Mi primera página")
 st.header("Hola, como va todo")
 st.write("Diferente tipo de letra")
 
-
 image = Image.open("gatopan.jpg")
 st.image(image, caption = "gatopan")
+
+texto = st.text_input("Escribe algo" , "Este es mi texto")
+st.write("El texto escrito es" , texto)
+
+col1, col2 = st.columns(2)
+
+with col1
+  st.subheader("Esta es la primera columna")
+  st.write("Las interfaces multimodales mejoran la experiencia de ususario")
+  resp = st.checkbox("Estoy de acuerdo")
+  if resp:
+    st.write("Correcto!")
+
+with col2
+  st.subheader("Esta es la segunda columna")
+  modo=st.radio("Que modalidad es la principal de tu interfaz", ("Visual","Auditiva","Táctil"))
+  if modo == "Visual":
+    st.write("La vista es fundamental para tu interfaz")
+  if modo == "Auditiva":
+    st.write("La audición es fundamental para tu interfaz")
+  if modo == "Táctil":
+    st.write("El tacto es fundamental para tu interfaz")
